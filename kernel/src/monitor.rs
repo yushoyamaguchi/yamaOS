6651ac48b19babee5a92e08871d7f87eb177b81c
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-28 13:47:57 +0900

    disable backspace when no input

--- kernel/src/monitor.rs
+++ kernel/src/monitor.rs
@@ -4,13 +4,33 @@ pub fn monitor(){
     let mut c = ' ';
     cons_putc('K');
     cons_putc('>');
+    unsafe { 
+        CONS.wpos=2 ;
+        CONS.rpos=2 ;
+    }
     while 1==1  {
         if c=='\n' {
             cons_putc('K');
             cons_putc('>');
+            unsafe { 
+                CONS.wpos=2 ;
+                CONS.rpos=2 ;
+            }
         }
+        
         c = getc();
-        if c==b'\x08' as char && unsafe { CONS.wpos  <= 1 } {
+        // Now, we don't consider input buffer overflow
+        if c==b'\x08' as char && unsafe { CONS.rpos  > 3 } {
+            unsafe{
+                CONS.rpos -= 2;
+                CONS.wpos -= 2;
+            }
+        }
+        else if c==b'\x08' as char && unsafe { CONS.rpos  <= 3 } {
+            unsafe{
+                CONS.rpos -= 1;
+                CONS.wpos -= 1;
+            }
             continue;
         }
         cons_putc(c);
